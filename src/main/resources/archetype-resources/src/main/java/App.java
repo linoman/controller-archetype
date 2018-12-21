5eae34292c58c2211b7cd3de634c849c654d107e
linoman <lino.urdiales@gmail.com>
2018-12-21 15:02:03 +0100

    Create project

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $net.linoman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
